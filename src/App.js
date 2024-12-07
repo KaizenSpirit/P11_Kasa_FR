e1f9ed393f56f30fa5cabd058975eaaf45b58583
Anthony Grayling <anthony.grayling.milliondoll1216@hotmail.fr>
2024-12-07 10:20:23 +0100

    feat : site config and site log added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src='./LOGO.png' alt='Logo du site'></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
